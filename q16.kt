c932cbadac88f6add104b86d633627ee1359c490
cybercode <rikkar86030@gmail.com>
2026-05-28 14:59:57 +0530

    temperature conversion

---
+++ q16.kt
@@ -0,0 +1,12 @@
+fun ctof(c: Double): Double {
+    return (c * 9/5) + 32
+}
+
+fun ftoc(f: Double): Double {
+    return (f - 32) * 5/9
+}
+
+fun main() {
+    println(cToF(0.0))
+    println(ftocc(32.0))
+}
